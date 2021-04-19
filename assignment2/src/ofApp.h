ef11856b02a8c1163cecd6b84fdd0182c0ef7f6d
Maria Alkhaja <38201407+mariaalkhaja@users.noreply.github.com>
2021-04-19 17:29:04 +0400

    Create ofApp.h

---
+++ assignment2/src/ofApp.h
@@ -0,0 +1,21 @@
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+	ofSoundPlayer sound;
+	ofSoundPlayer sound1;
+	ofSoundPlayer sound2;
+	ofSoundPlayer sound3;
+	ofSoundPlayer sound4;
+	ofVideoPlayer dance;
+
+
+	float* fft;
+	float* soundSpectrum;
+	int bands;
+};
